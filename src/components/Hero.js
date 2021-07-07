2539e35f535caeea2cf7095dec82505992c28c41
habib <habibdev96@gmail.com>
2021-07-07 00:57:06 -0600

    added bg patterns to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,39 @@
 import styled from 'styled-components';
 import { headingStyles, maxWidthLg, textStyles } from '../abstracts/Mixins';
-import heroImg from '../images/image-intro-desktop.jpg';
 import { Button } from './styledElements/Button';
+import heroImg from '../images/image-intro-desktop.jpg';
+import bgPatternLeft from '../images/bg-pattern-intro-left-desktop.svg';
+import bgPatternRight from '../images/bg-pattern-intro-right-desktop.svg';
 
 const Header = styled.header`
   background-color: var(--veryDarkViolet);
   height: 75vh;
+  position: relative;
+
+  .bg-pattern {
+    position: absolute;
+    pointer-events: none;
+    z-index: 10;
+  }
+
+  .bg-pattern-left {
+    top: 70%;
+    width: 15%;
+  }
+
+  .bg-pattern-right {
+    top: 0;
+    right: 0;
+    width: 30%;
+  }
 `;
 
 const Container = styled.div`
-  padding-top: 10rem;
   ${maxWidthLg}
+  padding: 10rem 2rem 0 2rem;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 5rem;
+  gap: 2rem;
   align-items: center;
 
   .info {
@@ -51,6 +71,8 @@ const Container = styled.div`
 const Hero = () => {
   return (
     <Header>
+      <img src={bgPatternLeft} className={`bg-pattern bg-pattern-left`} />
+      <img src={bgPatternRight} className={`bg-pattern bg-pattern-right`} />
       <Container>
         <div className='info'>
           <div className='underline'></div>
